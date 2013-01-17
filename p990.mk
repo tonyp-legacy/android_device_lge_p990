65c65baaf1423e2fe93a83238335be9dc073bc10
tonyp <text2tony@gmail.com>
2013-01-17 22:18:59 +0100

    include the kernel as prebuilt for now
    Conflicts:
    	p990.mk

--- p990.mk
+++ p990.mk
@@ -15,10 +15,27 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/fstab.p990:root/fstab.p990
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/charger:root/charger \
+    $(foreach f,$(wildcard $(LOCAL_PATH)/prebuilt/res/images/charger/*),$(f):root/res/images/charger/$(notdir $(f))) \
+    $(foreach f,$(wildcard $(LOCAL_PATH)/prebuilt/res/images/security/*),$(f):root/res/images/security/$(notdir $(f)))
+
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab \
     $(LOCAL_PATH)/init.vsnet:system/bin/init.vsnet \
     $(LOCAL_PATH)/init.vsnet-down:system/bin/init.vsnet-down \
     $(LOCAL_PATH)/gps_brcm_conf.xml:system/etc/gps_brcm_conf.xml
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/check_sdcard.sh:system/bin/check_sdcard.sh \
+    $(LOCAL_PATH)/lgdrm.img:system/bin/lgdrm.img
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel \
+    $(LOCAL_PATH)/prebuilt/bthid.ko:system/lib/modules/bthid.ko \
+    $(LOCAL_PATH)/prebuilt/scsi_wait_scan.ko:system/lib/modules/scsi_wait_scan.ko \
+    $(LOCAL_PATH)/prebuilt/tcp_bic.ko:system/lib/modules/tcp_bic.ko \
+    $(LOCAL_PATH)/prebuilt/tcp_htcp.ko:system/lib/modules/tcp_htcp.ko \
+    $(LOCAL_PATH)/prebuilt/wireless.ko:system/lib/modules/wireless.ko
 
 PRODUCT_PACKAGES += \
     lgcpversion
